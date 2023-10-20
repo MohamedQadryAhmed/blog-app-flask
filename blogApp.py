092e2b5b33cffca1ebd93ffb975ef75bef5fd889
mohamedqadryahmed <mohamedqadryahmed@gmail.com>
2023-10-20 18:13:06 +0300

    add layout and extends home page

--- blogApp.py
+++ blogApp.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask , render_template
+
+
 
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/home')
 def home():
-    return "<h1>Hello Flask!</h1>"
+    return render_template('home.html')
